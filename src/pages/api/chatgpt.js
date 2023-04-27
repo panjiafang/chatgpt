8685541d71ee3c7ba801281c8c344b4d2e6a0b74
panjiafang <panjiafang@le.com>
2023-04-27 20:00:17 +0800

    message

--- src/pages/api/chatgpt.js
+++ src/pages/api/chatgpt.js
@@ -1,4 +1,4 @@
-import { Configuration, OpenAIApi } from "openai";
+import { Configuration, OpenAIApi, ChatCompletionRequestMessage } from "openai";
 
 const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
@@ -6,26 +6,32 @@ const configuration = new Configuration({
 
 const openai = new OpenAIApi(configuration);
 
-// export default async function handler(messages, res) {
-//     const prompt = req.body.prompt;
-//     const response = await openai.createChatCompletion({
-//         model: "gpt-3.5-turbo",
-//         messages: messages,
-//     });
-
-//     res.status(200).json(response);
-// }
-
 export default async function handler(req, res) {
     console.log("handler:", req.body);
     const messages = JSON.parse(req.body);
     console.log("messages:", messages)
+    const message = {'role': 'user', 'message': 'What is your name?'};
     try {
         const response = await openai.createChatCompletion({
-            model: "gpt-3.5-turbo",
-            messages: messages,
-        }, { timeout: 10000 });
-        res.status(200).json(response);
+            'model': "gpt-3.5-turbo",
+            'messages': messages
+        }, { timeout: 30000, proxy: {host: "127.0.0.1", port: 7890} });
+        // const response = await openai.createCompletion({
+        //     model: "text-davinci-003",
+        //     prompt: `Suggest three names for an animal that is a superhero.
+        //     Animal: Cat
+        //     Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
+        //     Animal: Dog
+        //     Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
+        //     Animal: Elephant
+        //     Names:`,
+        //     temperature: 0.5,
+        // }, { timeout: 10000, proxy: {host: "127.0.0.1", port: 7890} });
+
+        // const response = await openai.listModels({ timeout: 10000, proxy: {host: "127.0.0.1", port: 7890} });
+
+        console.log(response.data)
+        res.status(200).json(response.data);
     } catch (err) {
         console.log(err);
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,42 +1,58 @@
-import Head from "next/head"
-
-
+import Head from "next/head";
 
 export default function Chat() {
+    const messages = [
+        {
+            role: "system",
+            content: "Your name is John",
+        },
+    ];
 
-  async function getChat() {
-    try {
-        console.log('getChat')
-        const response = await fetch('/api/chatgpt', {
-            method: 'POST',
-            body: JSON.stringify( [
-                    {
-                        "role": "system",
-                        "message": "Your name is John",
-                    },
-                    {
-                        "role": "user",
-                        "message": "What is your name?",
-                    }
-                ]
-            )
-        })
-        const data = await response.json()
-        console.log(data)
-    } catch(err) {
-        console.log(err)
+
+
+    async function getChat() {
+      console.log("getChat");
+        try {
+          const text = document.getElementById("input").value;
+          var message = {'role': 'user', 'content': text};
+          messages.push(message);
+
+          const response = await fetch("/api/chatgpt", {
+            method: "POST",
+            body: JSON.stringify(messages),
+          });
+
+          const data = await response.json();
+          // console.log(data.choices[0].message);
+
+          // var resMessage = data.choices[0].message;
+          // messages.push(resMessage)
+
+        } catch (err) {
+            console.log(err);
+            messages.pop();
+        }
     }
-}
 
-  return (
-    <div>
-      <Head>
-        <title>Chat</title>
-      </Head>
-      <main>
-        <h1>Chat</h1>
-        <button onClick={getChat}>Get Chat</button>
-      </main>
-    </div>
-  )
+    return (
+        <div>
+            <Head>
+                <title>Chat</title>
+            </Head>
+            <main>
+                <h1>Chat</h1>
+                <button onClick={getChat}>Get Chat</button>
+                <input type="text" id="input"/>
+
+                <div>
+                    {messages.map((message, index) => (
+                        <div key={index}>
+                            <span>{message.role}</span>
+                            <span>{message.content}</span>
+                        </div>
+                    ))}
+                </div>
+            </main>
+        </div>
+    );
 }
